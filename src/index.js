2bcb2c2b5ffb0ea72f02a36dbf5823e9dcb741ae
Janel Grant <janelgr5@gmail.com>
2018-01-30 12:36:11 -0500

    map

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-console.log("Hello from JavaScript");
-
 const mapboxgl = require("mapbox-gl");
 
-mapboxgl.accessToken = "YOUR API TOKEN HERE";
+const buildMarker = require("./marker");
+
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFuZWxncjUiLCJhIjoiY2pkMXVoY3hhMnBxajJxcXNjdnl0amhkNyJ9.dUmC4J2E_KrHhJWmf53iDw';
 
 const map = new mapboxgl.Map({
   container: "map",
@@ -10,3 +12,13 @@ const map = new mapboxgl.Map({
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+// const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+// new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map); 
+
+const marker = buildMarker("activities", [-74.009151, 40.705086]);
+marker.addTo(map);
